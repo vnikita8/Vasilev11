3c61bd8f7b597b73c031644ed48db1e66b613f87
Nikita <vnikita123456@gmail.com>
2021-11-17 01:13:31 +0700

    1

---
+++ Vasilev11/Song.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vasilev11
+{
+    internal class Song
+    {
+        string name; //название песни
+        string author; //автор песни
+        Song prev; //связь с предыдущей песней в списке
+
+        public Song() { }
+        public Song(string Name, string Author) 
+        { 
+            name = Name;
+            author = Author;
+            prev = null;
+        }
+        public Song(string Name, string Author, Song Prev)
+        {
+            name=Name;
+            author =Author; 
+            prev = Prev;
+        }
+
+        public void setName(string Name) //метод для заполнения поля name
+        {
+            name = Name;
+        }
+
+        public void setAuthor(string Author) //метод для заполнения поля author
+        {
+            author = Author;
+        }
+        public void setPrev(Song song) //метод для заполнения поля prev
+        {
+            prev = song;
+        }
+              
+        public string Title() //метод для печати названия песни и ее исполнителя
+        {
+            return $"{name} - {author}";
+        }
+
+        public override bool Equals(Object d) //метод, который сравнивает между собой два объекта-песни
+        {
+            if (d.GetType() == GetType())
+                return true;
+            else
+                return false;
+        }
+    }
+}
